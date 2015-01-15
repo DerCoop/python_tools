282b13511268d6f524a93a9c09e855a35eb8219c
Der Coop <dercoop@users.sourceforge.net>
2015-01-15 11:31:25 +0100

    add gawril main draft

---
+++ gpx/gawril.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python
+
+"""gawril is a smart tool to manipulate gpx files
+
+* split one track into segments
+* show gpx info
+
+for short, it is an user API for gpxpy with some functionality which I needed
+for my training
+"""
+
+__author__ = 'DerCoop'
+
+
+def gawril():
+
+    pass
+
+
+if __name__ == '__main__':
+    gawril()

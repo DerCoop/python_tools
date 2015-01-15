abfb84284b8c15f732a80fbe281dc508c1357afe
Der Coop <dercoop@users.sourceforge.net>
2015-01-15 16:00:50 +0100

    add commandline parser

--- gpx/gawril.py
+++ gpx/gawril.py
@@ -12,10 +12,19 @@ for my training
 __author__ = 'DerCoop'
 
 
-def parse_clo():
-    """parse the command line options"""
-    pass
 
+def get_cli_options():
+    """returns a pair (values, args) of the command line options"""
+    import argparse
+    parser = argparse.ArgumentParser(description='gawril - manipulate your gpx files')
+    parser.add_argument('-I', '--input-file', action='store', type=argparse.FileType('r'),
+                        required=True,
+                        help='the gpx source file')
+    parser.add_argument('-S', '--segments', action='store',
+                        help='defined segments, comma separated list with the format: '
+                             '(type:value,type:value)')
+    parser.add_argument('-2', '--2D', action='store_true',
+                        help='use only 2 dimensions')
 
 def split_track(segments):
     """split the track into segments
